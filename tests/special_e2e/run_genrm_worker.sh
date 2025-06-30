c0c44703a58d393f86788fd4d00270ecf5edfc2d
yuyangding <yuyangding@bytedance.com>
2025-06-30 21:43:26 +0800

    add ci test

---
+++ tests/special_e2e/run_genrm_worker.sh
@@ -0,0 +1,46 @@
+set -x
+
+python3 -m recipe.genrm_worker.genrm_main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=${HOME}/data/gsm8k/train.parquet \
+    data.val_files=${HOME}/data/gsm8k/test.parquet \
+    data.train_batch_size=256 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=2048 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-0.5B-Instruct \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=sglang \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.n=4 \
+    algorithm.use_kl_in_reward=False \
+    reward_model.enable=True \
+    reward_model.model.path=dyyyyyyyy/Qwen2.5-1.5B-GenRM-WithTemplate \
+    reward_model.vllm_infer.temperature=0.7 \
+    reward_model.vllm_infer.top_p=0.95 \
+    reward_model.vllm_infer.prompt_length=4096 \
+    reward_model.vllm_infer.response_length=4096 \
+    reward_model.vllm_infer.gpu_memory_utilization=0.8 \
+    reward_model.vllm_infer.tensor_model_parallel_size=1 \
+    reward_model.vllm_infer.n=1 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console'] \
+    trainer.project_name='verl-test' \
+    trainer.experiment_name='qwen2.5-0.5b-gen-rm-worker' \
+    trainer.n_gpus_per_node=8 \
+    trainer.val_before_train=False \
+    trainer.nnodes=1 \
+    trainer.save_freq=20 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=10 \
+    trainer.resume_mode='disable' \
+    trainer.total_training_steps=2
